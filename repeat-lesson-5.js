8439bd08909d37b58b27735ba760880090d2dce7
Aleksey <Coppola80@mail.ru>
2022-11-09 21:01:26 +0500

    added first commit

---
+++ repeat-lesson-5.js
@@ -0,0 +1,416 @@
+// 1. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(100 > 59); // true
+// console.log(10 < 7); // false
+// console.log(8 <= 8); // true
+// console.log(10 !== 9); // true
+// console.log(77 === 77); // true
+
+// console.log('test' === 'test'); // true
+// console.log('test' === 'Test'); // false
+// console.log('tester' === 'tes1er'); // false
+// console.log('string' !== 'String'); // true
+// console.log('px' !== 'mm'); // true
+
+// =============================================================== //
+
+// 2. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// let num = "10"
+// console.log(typeof num === "number"); // false
+// console.log(typeof num); // string
+// console.log(typeof "number"); // string
+
+// =============================================================== //
+
+// 3. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+//let text = "Victor"
+//console.log(typeof text === "String"); // false Потому что маленькие буквы имеют больший приоритет в таблице unicode чем заглавные.
+//console.log(typeof text); // string
+//console.log("String"); // string
+
+// =============================================================== //
+
+// 4. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// let isTest = true;
+// console.log(typeof isTest === "boolian"); // false
+// console.log(typeof isTest); // boolean
+// console.log("boolian"); // boolian
+
+
+// =============================================================== //
+
+// 5. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// let num = 10;
+// console.log(typeof (typeof isTest) === "number"); // false
+// console.log(typeof (typeof isTest)); // string
+// console.log(typeof "number"); // string
+
+// =============================================================== //
+
+// 6. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(Number("0")); // 0
+// console.log(Number("10")); // 10
+// console.log(Number("")); // 0
+// console.log(Number(null)); // 0
+// console.log(Number("  ")); // 0
+// console.log(Number(true)); // 1
+
+// console.log(Number("20px")); // NaN
+// console.log(Number(undefined)); // NaN
+// console.log(Number(false)); // 0
+// console.log(Number("true")); // NaN
+// console.log(Number("00100")); // 100
+// console.log(Number(" ")); // 0
+
+// =============================================================== //
+
+// 7. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// let a = 20;
+// let b = "20";
+
+// console.log(a === b); // false
+// console.log(a == b); // true
+// console.log(a >= b); // true
+// console.log(a <= b); // true
+// console.log(a != b); // false
+
+// console.log(typeof a); // number
+// console.log(typeof b); // string
+
+// console.log(a !== b); // true
+
+// =============================================================== //
+
+// 8. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// let a = 22;
+// let b = 12;
+
+// console.log(a === b); // false
+// console.log(a == b); // false
+// console.log(a >= b); // true
+// console.log(a <= b); // false
+// console.log(a != b); // true
+// console.log(a !== b); // true
+
+// =============================================================== //
+
+// 9. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// let c = 55;
+// let b = "55";
+
+// console.log(c === b); // false
+// console.log(c == b); // true
+// console.log(c >= b); // true
+// console.log(c <= b); // true
+// console.log(c != b); // false
+// console.log(c !== b); // true
+
+// =============================================================== //
+
+// 10. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(NaN == NaN); // false
+// console.log(NaN === NaN); // false
+
+// console.log(false == "false"); // false
+// console.log(Boolean(false)); // false
+// console.log(Boolean('false')); // true
+// console.log(Number('false')); // NaN
+// console.log(Number(false)); // Number 0
+
+// console.log(true == "true"); // false
+// console.log(isNaN(10 + "5")); // false
+
+// console.log(isNaN(10 - "5")); // false
+// console.log(isNaN(false)); // false
+// console.log("" == false); // true
+// console.log("0" == false); // true
+// console.log(10 == true); // false
+
+// =============================================================== //
+
+// 11. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(null == undefined); // true
+// console.log('Кит' < 'Киндер'); //   Т больше по unicode Н поэтому false
+// console.log(0 == ""); // true
+// console.log('Я' > 'Ю'); // true
+
+// //         object   boolean
+// console.log(null == false); //  Два разных типа данных при нестрогом равенстве = false
+// console.log(null === false); //  Два разных типа данных при строгом равенстве = false
+// console.log('  ' == 0); //  false
+
+// console.log(typeof false); // boolean
+// console.log(typeof null); // object
+
+// console.log(Number(false)); // 0
+// console.log(Number(null)); // 0
+// console.log(Boolean(null)); // false
+
+// console.log(Number({})); // NaN
+// console.log(isNaN(Number({}))); // true
+
+// console.log(null == 0); // true
+
+// =================================================
+//                    0
+// console.log(isNaN(10 - "10") == 1); // false
+// console.log(isNaN(10 + "abc") == 1); // true
+// console.log(10 - "10"); // Number 0
+// console.log(isNaN(0 == 1)); // false
+
+
+// console.log("" == false); // true
+// console.log(null === undefined); // false
+// console.log(17 >= 17); // true
+// console.log('U' > 'Z'); // false
+// console.log(undefined === undefined); // true
+// =============================================================== //
+
+// 12. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(false == 0); // true
+
+// //         Number 20   string
+// console.log(15 + 5 === "20"); // false
+// console.log(15 <= "15"); // true
+// console.log("t" == 1); // false
+// console.log('2' > 1); // true
+// console.log(5 - '2' > '1'); // true
+
+// console.log(17 + 3 == 21 - "1"); // true
+// console.log(" " == 0); // true
+// console.log("05" > 4); // true
+
+// //         Number 53   String 4013
+// console.log(100 - 47 == 40 + "13"); // false
+
+// console.log(true == -1); // false
+
+// //          Number 0    Number 0
+// console.log(10 - '10' >= null); // true
+// =============================================================== //
+
+// 13. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+
+//              true              true
+// console.log(17 + 3 === 20 && 20 - 3 == "17"); // true
+
+
+
+
+//                true                    true
+//                                  !true      true           !true
+//                true                                    false
+// console.log(50 + 50 == "100" || (!false && !NaN && !(27 + 3 === "30"))); // true
+
+// console.log(typeof !NaN); // boolean
+// console.log(typeof NaN); // Number
+// console.log(Boolean(!NaN)); // true
+// console.log(Boolean(NaN)); // false
+
+
+// console.log(Number("100") === 100); // true
+
+//          string         String
+// console.log(typeof "" === "String"); // false
+// console.log(typeof ''); // string
+
+
+// console.log(Number(String(10) + 0) === 100); // true
+
+// console.log(typeof false === "Boolean"); // false
+// console.log(50 + String(Number(Boolean(null))) == 500 || false); // true
+
+
+// console.log(!"" && !null && !(2 + 2 * 2 === 8)); // true
+// console.log(Boolean('')); // false
+// console.log(Boolean(!'')); // false
+
+// console.log(Boolean(null)); // false
+// console.log(Boolean(!null)); // true
+
+
+
+// console.log(5 > 3 && 7 < 10 && 5 == "5"); // true
+// console.log(typeof true == "boolean"); // true
+// =============================================================== //
+
+// 14. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(true || false); // true
+// console.log(false && true); // false
+// console.log(false || true && true); // true
+// console.log(false || true && false); // false
+// console.log(false && true || true); // true
+// console.log(false && true || true && (false || true)); // true
+
+// console.log(6 > 3 || 4 < 1); // true
+// console.log(6 >= 6 && 10 < 15); // true
+// console.log(6 > 9 || 5 > 3 && 7 > 4); // true
+// console.log(6 > 9 || 1 > 3 && 7 > 4); // false
+
+// console.log(10 != 10 && 5 == 5 || -5 > -9); // true
+// console.log(10 != 10); // false
+// console.log(10 !== 10); // false
+
+// console.log(7 >= 9 && 5 == 5 || 7 > 3 && (14 < 10 || 10 > 9)); // true
+// =============================================================== //
+
+// 15. Задача
+// Измените приоритет действий, чтобы в консоль вывелась истина.
+
+// console.log(((72 / 12) + 6) * 4 === 48); // true
+// console.log((72 / 12) + (6 * 4) === 30); // true
+// console.log((72 / (12 + 6)) * 4 === 16); // true
+// console.log(72 / (12 + 6 * 4) === 2); // true
+// console.log((120 - 40) / 5 - 2 === 14); // true
+// console.log((72000 / 800) / (5 + 40) === 2); // true
+
+// console.log(false == null || true == 1 && 0 === ""); // false
+// console.log(false == null); // false
+// console.log(true == 1); // true
+// console.log(0 === ''); // false
+// console.log(typeof 0); // number
+// console.log(typeof ''); // string
+
+// =============================================================== //
+
+// 16. Задача
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(Boolean("")); // false
+// console.log(Boolean("false")); // true
+// console.log(Boolean(NaN)); // false
+// console.log(Boolean(100)); // true
+// console.log(Boolean(0)); // false
+// console.log(Boolean(null)); // false
+
+// console.log(Boolean(-1)); // true
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean(0)); // false
+// console.log(Boolean("0")); // true
+// console.log(Boolean(" ")); // true
+// =============================================================== //
+
+// ★ Задачи повышенной сложности ★
+// 17. ★ Задача ★
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(3 && 2); // 2 Возвращает последнею истину
+// console.log(0 && 2); // 0 Возвращает первую ложь
+// console.log(2 && 0); // 0 Последняя ложь
+// console.log(12 && 9); // 9 Последняя истина
+
+// console.log(1 || 0); // 1 Возращает первую истину
+// console.log(1 < 0 || 1 > 0); // true. Возращает первую истину
+// console.log(1 > 0 || 1 < 0); // true. Возращает первую истину
+// console.log('A' || 'a'); // A. Возращает первую истину
+// console.log('a' || 'B'); // a. Возращает первую истину
+// console.log('' || 'B'); // B. Возращает последнею истину
+// console.log('b' || ''); // b. Возращает первую истину
+
+// console.log(4 && !0); // 1
+// console.log(!null && !""); // true
+// console.log(typeof !null); // boolean
+// console.log(typeof !''); // boolean
+
+// console.log(0 || true && 1); // 1
+// console.log(0 || true); // true
+
+// console.log(true && true || false); // true
+// console.log(1 && 1 || 0); // 1
+// console.log('t' && 'a' || ''); // a
+// console.log(String('a').charCodeAt('0')); // Number 97
+// console.log(String('t').charCodeAt('0')); // Number 116
+// console.log(String('').charCodeAt('')); // NaN
+
+// console.log('t' && 'a' || '' && NaN); // a
+// console.log(Number('t')); // NaN
+// console.log(isNaN(Number('t'))); // True
+
+// console.log('t' && !'' || !'' && !NaN); // true
+// =============================================================== //
+
+// 18. ★ Задача ★
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// const serverLogin = '';
+// const answer = serverLogin || 'default log';
+// console.log(answer); // Последняя истина ==='default log'
+// =============================================================== //
+
+// 19. ★ Задача ★
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(5 > 3 && 7 < 10 && 5 == "5" || false && "1");
+// console.log(5 > 3 && 7 < 10 && 5 == "5" && false || "1");
+// console.log(3 && 5 && null && 4);
+// console.log(1 && 0 && 10 && 3);
+// console.log(1 && 100 && 2 && 0);
+// console.log(1 && 100 && 2 && 50);
+
+// console.log(null || 0 || undefined);
+// console.log(1 || 0 || true || false);
+// console.log(0 || 0 || 0 || 0 || 1);
+// console.log((0 || 0 || 2) && ("" || NaN));
+// console.log(null || 0 || false || undefined || "" || -1 || NaN || 1);
+// =============================================================== //
+
+// 20. ★ Задача ★
+// Не выполняя код, дать ответ:
+// Что выведется в консоль ? Ответ записать в комментарий.
+
+// console.log(!!null || !!!false && !"");
+// console.log(5 > 3 && 1 && !null);
+// console.log('text' || 5 > 3 || null);
+// console.log(!'text' || 5 > 3 || undefined);
+// console.log(undefined && false && null && 0);
+
+// console.log(false && 'image');
+// console.log(!0 && !null && !undefined && !NaN && !' ' && !false && !'');
+// console.log(!!!false && (!!!true || !!!false));
+// console.log('false' && true && !'' && true);
+// console.log('false' || true || !'' || true);
